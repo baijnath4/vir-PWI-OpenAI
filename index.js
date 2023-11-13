05f836efcb6601dfcf14dfb0e05e87e0462635c6
Baijnath4kumar <baijnath4kumar@gmail.com>
2023-11-13 12:49:30 +0530

    first commit

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const cors = require('cors');
 
 const fs = require('fs');
 const jsYaml = require('js-yaml');
+
+// port randomly assigned during deployment so use process.env.port 
+const PORT = process.env.PORT || 3030; 
 // const swaggerUI = require("swagger-ui-express")
 // const swaggerJsDoc = require("swagger-jsdoc")
 
@@ -63,6 +66,6 @@ app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(specs))
 app.use("/",apiRouter)
 
 
-app.listen(8000, () => {
-  console.log(`Server is running on port 3000`);
+app.listen(PORT , () => {
+  console.log(`Server is running on port ${PORT}`);
 });
